644c2df52b59e4c5a3c18a78873c6d8509c010fc
nicosanfurgo <nicosanmza@hotmail.com>
2023-08-29 11:02:58 -0300

    Arreglos

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const apiKey = "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";
+const apiKey = "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";
 
-const maxImages = 4; 
+const maxImages = 4; // Number of images to generate for each prompt
 let selectedImageNumber = null;
 
-
-function getRandomNumber(min, max){
-    return Math.floor(Math.random () * (max - min + 1)) + min;
+// Function to generate a random number between min and max (inclusive)
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//Function to disable the generate button during procesing
-function disableGenerateButton(){
+// Function to disable the generate button during processing
+function disableGenerateButton() {
     document.getElementById("generate").disabled = true;
 }
 
-//Function to enable the generate button after process
-function enableGenerateButton(){
+// Function to enable the generate button after process
+function enableGenerateButton() {
     document.getElementById("generate").disabled = false;
 }
 
-//Function to clear image grid
-function clearImageGrid(){
+// Function to clear image grid
+function clearImageGrid() {
     const imageGrid = document.getElementById("image-grid");
     imageGrid.innerHTML = "";
 }
 
-//Function to generate images
-async function generateImages(input){
+// Function to generate images
+async function generateImages(input) {
     disableGenerateButton();
     clearImageGrid();
 
@@ -34,24 +34,24 @@ async function generateImages(input){
 
     const imageUrls = [];
 
-    for(let i = 0; i < maxImages; i++){
-        //generate a random number between 1 and 10000 and append it to the prompt
-        const randomNumber = getRandomNumber(1,10000);
-        const prompt = '${input} ${randomNumber}';
-        //We added random number to prompt to create different results
+    for (let i = 0; i < maxImages; i++) {
+        // Generate a random number between 1 and 10000 and append it to the prompt
+        const randomNumber = getRandomNumber(1, 10000);
+        const prompt = `${input} ${randomNumber}`;
+        // We added random number to prompt to create different results
         const response = await fetch(
             "https://api-inference.huggingface.co/models/prompthero/openjourney",
             {
                 method: "POST",
-                headers:{
+                headers: {
                     "Content-Type": "application/json",
-                    "Authorization": 'Bearer ${apiKey}',
+                    "Authorization": `Bearer ${apiKey}`,
                 },
-                body: JSON.stringify({ inputs: prompt}),
+                body: JSON.stringify({ inputs: prompt }),
             }
         );
-        
-        if(!response.ok){
+
+        if (!response.ok) {
             alert("Failed to generate image!");
         }
 
@@ -61,26 +61,26 @@ async function generateImages(input){
 
         const img = document.createElement("img");
         img.src = imgUrl;
-        img.alt = 'art-${i + 1}';
+        img.alt = `art-${i + 1}`;
         img.onclick = () => downloadImage(imgUrl, i);
-        document.getElementById("image-grid").appendChild (img);
+        document.getElementById("image-grid").appendChild(img);
     }
 
     loading.style.display = "none";
     enableGenerateButton();
 
-    selectedImageNumber = null; //Reset selected image number
+    selectedImageNumber = null; // Reset selected image number
 }
 
-document.getElementById("generate").addEventListener('click', () =>{
+document.getElementById("generate").addEventListener('click', () => {
     const input = document.getElementById("user-prompt").value;
     generateImages(input);
 });
 
-function downloadImage(imgUrl, imageNumber){
+function downloadImage(imgUrl, imageNumber) {
     const link = document.createElement("a");
     link.href = imgUrl;
-    //set filename based on the selected image
-    link.download = 'image-${imageNumber + 1}.jpg';
+    // Set filename based on the selected image
+    link.download = `image-${imageNumber + 1}.jpg`;
     link.click();
 }
